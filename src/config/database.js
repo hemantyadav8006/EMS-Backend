dd11797886f0384f01c9849a1564670a9620202d
hemantyadav8006 <hemant.kumar@kollegeapply.com>
2025-04-16 11:05:23 +0530

    fix: update database connection logging and enhance server startup message

--- src/config/database.js
+++ src/config/database.js
@@ -13,3 +13,12 @@ export const sequelize = new Sequelize(
     logging: true,
   }
 );
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Database connection has been established successfully.");
+  } catch (error) {
+    console.error("❌ Unable to connect to the database:", error);
+  }
+})();
--- src/index.js
+++ src/index.js
@@ -28,5 +28,5 @@ app.use("/api/v1/users", loginUsers);
 
 const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port http://localhost:${PORT}`);
 });
