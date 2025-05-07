27a35299ff3760f4f34316844c06c63c84f918a6
hemantyadav8006 <hemant.kumar@kollegeapply.com>
2025-05-07 12:51:32 +0530

    unlink the file from public folder

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,8 +16,9 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    // console.log(response);
+
     console.log("File is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved file when upload fails
